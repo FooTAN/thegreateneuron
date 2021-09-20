1e031396565a8c218621fc472bc7edd403dea494
tan <tan@localhost>
2021-09-21 01:58:32 +0200

    setting up admin and client

---
+++ src/services/client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Go To Admin.
+        </p>
+        <a className="App-link" href="http://localhost/admin"> Admin </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
