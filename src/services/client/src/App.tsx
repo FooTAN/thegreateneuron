1625b5c865466c011c06419f28610af64883dc4a
tan <tan@localhost>
2021-09-22 08:06:28 +0200

    client changes

--- src/services/client/src/App.tsx
+++ src/services/client/src/App.tsx
@@ -5,9 +5,74 @@ import GotoSignup from './components/GotoSignup';
 
 function App() {
   return (
-    <div className="container">
-      <GotoSignup></GotoSignup>
+    <div>
+<div className="container py-3">
+  <header>
+    <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
+        <span className="fs-4">The Great Neuron</span>
+      </a>
+
+      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a className="me-3 py-2 text-dark text-decoration-none" href="/admin/signup/">TmpAdmin</a>
+        <a className="py-2 text-dark text-decoration-none" href="#">Login</a>
+      </nav>
     </div>
+  </header>
+
+  <main>
+  <section className="py-5 text-center container">
+    <div className="row py-lg-5">
+      <div className="col-lg-6 col-md-8 mx-auto">
+        <h1 className="fw-light">Latest From TGN</h1>
+        <p className="lead text-muted">Some Text here</p>
+        <p>
+          Maybe more text here
+        </p>
+      </div>
+    </div>
+  </section>
+    <h2 className="display-6 text-center mb-4">Latest Dog vs Cat Comparison</h2>
+</main>
+
+  <footer className="pt-4 my-md-5 pt-md-5 border-top">
+    <div className="row">
+      <div className="col-12 col-md">
+        <small className="d-block mb-3 text-muted">&copy; 2021</small>
+      </div>
+      <div className="col-6 col-md">
+        <h5>Features</h5>
+        <ul className="list-unstyled text-small">
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Random feature</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Team feature</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Another one</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div className="col-6 col-md">
+        <h5>Resources</h5>
+        <ul className="list-unstyled text-small">
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Resource</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Resource name</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Another resource</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div className="col-6 col-md">
+        <h5>About</h5>
+        <ul className="list-unstyled text-small">
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Team</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Locations</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Privacy</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</div>
+</div>
   );
 }
 
