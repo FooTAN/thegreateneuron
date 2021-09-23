27972c6049aa14b571dadfeb692065a055f3f6df
tan <tan@localhost>
2021-09-24 01:25:55 +0200

    added footer component

--- src/services/client/src/layout/NavBar/index.tsx
+++ src/services/client/src/layout/NavBar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function index() {
+function NavBar() {
     
     return (
         <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
@@ -16,4 +16,4 @@ function index() {
     )
 }
 
-export default index
+export default NavBar
