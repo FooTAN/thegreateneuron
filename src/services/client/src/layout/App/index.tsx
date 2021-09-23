c2df943a1bd04398f7b41d22d831b8abf8ca3775
tan <tan@localhost>
2021-09-24 01:19:59 +0200

    added navbar component

--- src/services/client/src/layout/App/index.tsx
+++ src/services/client/src/layout/App/index.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import './style.scss';
 import UsersView from '../../components/UsersView';
 import ArticleFeaturette from '../../components/ArticleFeaturette';
+import NavBar from '../NavBar';
 
 function App() {
   return (
     <div>
 <div className="container py-3">
   <header>
-    <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
-        <span className="fs-4">The Great Neuron</span>
-      </a>
-
-      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <a className="me-3 py-2 text-dark text-decoration-none" href="/admin/signup/">TmpAdmin</a>
-        <a className="py-2 text-dark text-decoration-none" href="#">Login</a>
-      </nav>
-    </div>
+    <NavBar/>
   </header>
 
   <main>
--- src/services/client/src/layout/NavBar/index.tsx
+++ src/services/client/src/layout/NavBar/index.tsx
@@ -3,9 +3,16 @@ import React from 'react'
 function index() {
     
     return (
-        <div>
-            
-        </div>
+        <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+            <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
+            <span className="fs-4">The Great Neuron</span>
+            </a>
+  
+            <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+            <a className="me-3 py-2 text-dark text-decoration-none" href="/admin/signup/">TmpAdmin</a>
+            <a className="py-2 text-dark text-decoration-none" href="#">Login</a>
+            </nav>
+      </div>
     )
 }
 
