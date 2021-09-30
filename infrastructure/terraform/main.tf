0a563ec869e9737da756e8e87166bb63148e3600
tan <tan@localhost>
2021-09-30 09:13:14 +0200

    restoring previous change

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -19,3 +19,19 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
 }
 
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.cluster_node_count
+    vm_size    = var.cluster_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
