7af3f91ffe43a2ff3d61905185860309b7085ebe
tan <tan@localhost>
2021-09-30 09:09:32 +0200

    temp change

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,7 +5,9 @@ terraform {
       version = "=2.78.0"
     }
   }
-  
+
+  backend "azurerm" {
+  }  
 }
 
 provider "azurerm" {
@@ -17,19 +19,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = var.cluster_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.cluster_dns_prefix
-
-  default_node_pool {
-    name       = "default"
-    node_count = var.cluster_node_count
-    vm_size    = var.cluster_vm_size
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
