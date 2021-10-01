110aa0e2f4f625946f70dfcabef9af2dfe5b9468
tan <tan@localhost>
2021-10-01 15:41:05 +0200

    maintenance

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     name       = "default"
     node_count = var.cluster_node_count
     vm_size    = var.cluster_vm_size
+    os_type = "Linux"
   }
 
   identity {
@@ -39,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     http_application_routing {
       enabled = true
     }
-
+# let us disable som addons, otherwise we'll automatically get unecessary provisioning
     aci_connector_linux {
       enabled = false
     }
