d0cfcd2f7355c91734361ede1c82225e320c24f8
tan <tan@localhost>
2021-10-07 02:20:10 +0200

    maintenance

--- src/services/article/src/app.ts
+++ src/services/article/src/app.ts
@@ -10,10 +10,6 @@ const app = express();
 app.set('trust proxy', true);
 
 app.use(express.json());
-app.use(cookieSession({
-  signed: false,
-  secure: process.env.NODE_ENV !== 'test',
-}));
 
 app.use(articlesRouter);
 
